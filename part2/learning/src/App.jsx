f4bbf79523d53fe40d304dfa60739401cae0db85
Watashi253 <ananditagupta253@gmail.com>
2026-05-31 20:41:01 +0530

    made few changes in frontend

--- part2/learning/src/App.jsx
+++ part2/learning/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Footer from './components/Footer'
 import Notes from './components/Notes.jsx'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -34,8 +35,8 @@ const App = () => {
 
     noteService
       .create(noteObject)
-      .then(initialNotes => {
-        setNotes(initialNotes)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
 
@@ -48,19 +49,21 @@ const App = () => {
 
   const toggleImportanceOf = (id) => {
     console.log('importance of ' + id + ' needs to be toggled')
-    const url = `/api/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
-        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -74,11 +77,13 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note =>
-          <Notes key={note.id}
+        {notesToShow.map(note => (
+          <Notes
+            key={note.id}
             note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)} />
-        )}
+            toggleImportance={() => toggleImportanceOf(note.id)} 
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
         <input
@@ -87,6 +92,7 @@ const App = () => {
           placeholder='a new note...' />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
